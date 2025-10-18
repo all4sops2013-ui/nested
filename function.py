55534a96e2212ff387e9c81a34b4c8b142b90a2e
all4sops2013-ui <all4sops2013@gmail.com>
2025-10-18 13:33:33 +0100

    Add files via upload

---
+++ function.py
@@ -0,0 +1,30 @@
+def wish(n):
+  print("hello, welcome to my class...",n, "you age is", age)
+
+age = 14 # global variables
+name1 = input("Enter your name:")
+wish(name1)
+
+
+
+def wish(n):
+  print("hello, welcome to my class...",n)
+
+
+
+name1 = input("enter your name:")
+wish(name1)
+
+name1 = input("enter your name:")
+wish(name1)
+
+name1 = input("enter your name:")
+wish(name1)
+
+name1 = input("enter your name:")
+wish(name1)
+
+name1 = input("enter your name:")
+wish(name1)
+
+wish(name1)
